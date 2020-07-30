66a3c447b2cefd5499f764270d2246a6a31d9d6c
tomoyatakahashi0909 <s1842084tr@s.chibakoudai.jp>
2020-07-30 16:48:06 +0900

    test

--- htdocs/m_config.php
+++ htdocs/m_config.php
@@ -3,10 +3,10 @@
 Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password)
 */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
+define('DB_SERVER', '127.0.0.1');
+define('DB_USERNAME', 'testuser');
 define('DB_PASSWORD', 'pass');
-define('DB_NAME', 'shimodab1');
+define('DB_NAME', 'mydb');
 
 try{
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
